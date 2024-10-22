5e7d1d1138eaad9786f40cac13b497513ddad7f4
Aditya Bhattad <adityabhattad18@gmail.com>
2024-10-23 02:04:49 +0530

    update to latest

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -6,9 +6,11 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    "mantle-testnet": {
-      url: "https://rpc.testnet.mantle.xyz/",
-      accounts: [process.env.PRIVATE_KEY]
+    anvil:{
+      url:"http://localhost:8545",
+      accounts:["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
+      chainId:31337,
+      blockConfirmations:1,
     },
     hardhat: {
 			chainId: 1337,
